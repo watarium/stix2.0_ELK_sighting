624731dc5da6a551af0bef534cba3de18612373c
watarium <watrvir@hotmail.com>
2018-01-01 20:10:24 +0900

    Delete old files

--- csv2stix.py
+++ csv2stix.py
@@ -1,17 +1,16 @@
-# Please use exported csv file from Kibana visualization
+# Please use exported csv file from Kibana visualization that include at least @timestamp, request.keyword and count.
 
 from datetime import datetime
 from stix2 import Sighting
-import pandas as pd
 from os import path
 import sys
 import csv
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch(sys.argv[1])
-df = pd.DataFrame(columns = ['id','time','request','count'])
 
 def readcsvfile(csvfile):
+    datalist = []
     current_dir = path.dirname(__file__)
     f = open(path.join(current_dir, csvfile),'r',encoding='utf-8')
     reader = csv.reader(f)
@@ -26,30 +25,40 @@ def readcsvfile(csvfile):
             countn = n
 
     for row in reader:
-        creatdf(row[timen], row[reqestn], row[countn])
-    createsighting(df)
+        if getid(row[reqestn]) != None:
+            datalist.append([getid(row[reqestn]), int(row[timen]), row[reqestn], int(row[countn])])
+        else:
+            print(row[reqestn] + ' is not on STIX index.')
+    createdata(datalist)
     f.close()
 
-def creatdf(timev, requestv, countv):
-    global df
-    df = df.append(pd.Series([getid(requestv),timev,requestv,int(countv)], index=df.columns), ignore_index = True)
+def createdata(datalist):
+    firstseen = 9999999999999
+    lastseen = 0000000000000
+    id = datalist[0][0]
+    count = 0
+    nextlist = []
 
-def createsighting(df):
-    firstseen = df.groupby('id')['time'].min()
-    lastseen = df.groupby('id')['time'].max()
-    totalcount = df.groupby('id')['count'].sum()
-    print(firstseen,lastseen,totalcount)
+    for dataone in datalist:
+        if id == dataone[0]:
+            if firstseen > dataone[1]:
+                firstseen = dataone[1]
+            if lastseen < dataone[1]:
+                lastseen = dataone[1]
+            count = count + dataone[3]
+        else:
+            nextlist.append(dataone)
+    createsighting(id,firstseen,lastseen,count)
+    if not nextlist == []:
+        createdata(nextlist)
 
-def extract_data(df):
-    print(df.pivot_table(df,index='id',columns='time'))
-    # unixtime = row[0]
-    # clientip = row[1]
-    # patterninfo = row[2]
-    # validtime = datetime.utcfromtimestamp(int(str(unixtime)[0:10])).isoformat() + '.000Z'
-    # sighting = Sighting(name="test",
-    #                       labels=["malicious-activity"],
-    #                       pattern="[domain-name:value = " + patterninfo+ "]",
-    #                       valid_from=validtime)
+
+def createsighting(id,firstseen,lastseen,count):
+    firstseen = createzulu(firstseen)
+    lastseen = createzulu(lastseen)
+    sighting = Sighting(sighting_of_ref=id,first_seen=firstseen,last_seen=lastseen,count=count)
+    print(sighting)
+
 
 def getid(requestv):
     # extract domain or IP address information from stix index
@@ -59,13 +68,16 @@ def getid(requestv):
             stixdomain = hit['_source']['domain']
             if stixdomain in requestv:
                 id = hit['_source']['id']
-
         if 'IPaddress' in hit['_source']:
             stixdomain = hit['_source']['IPaddress']
             if stixdomain in requestv:
                 id = hit['_source']['id']
+    if 'id' in locals():
+        return id
 
-    return id
+def createzulu(unixtime):
+    validtime = datetime.utcfromtimestamp(int(str(unixtime)[0:10])).isoformat() + '.000Z'
+    return validtime
 
 if __name__ == '__main__':
     readcsvfile(sys.argv[2])
